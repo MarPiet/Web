c60b0e4e216796bd68ec0337980d1842ddbbe15e
Maz <markuspietila95@gmail.com>
2020-03-17 15:23:21 +0200

    Updates

---
+++ precache-manifest.f1a1d9e74b3298b8e7282ddfab1da1be.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf0ae39b547d47c5b7c25b9a2c61befb",
+    "url": "/Web/index.html"
+  },
+  {
+    "revision": "b4adebfe091f4e6471b2",
+    "url": "/Web/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "969b7c5b4f426c4a56bf",
+    "url": "/Web/static/css/main.528b24ac.chunk.css"
+  },
+  {
+    "revision": "b4adebfe091f4e6471b2",
+    "url": "/Web/static/js/2.41b42561.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/Web/static/js/2.41b42561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "969b7c5b4f426c4a56bf",
+    "url": "/Web/static/js/main.d0f05e26.chunk.js"
+  },
+  {
+    "revision": "ecd83c6e3531eb748e79",
+    "url": "/Web/static/js/runtime-main.e474bf14.js"
+  },
+  {
+    "revision": "7350072e72fd5b0cfd066f399d2fe2f5",
+    "url": "/Web/static/media/KeyFinder.7350072e.png"
+  },
+  {
+    "revision": "44e0b04a8c20c8452aba3b6c1bd3a776",
+    "url": "/Web/static/media/LaserGrid.44e0b04a.png"
+  },
+  {
+    "revision": "0c126ba9d65067be9f664e963e5dc2cf",
+    "url": "/Web/static/media/Relaatio.0c126ba9.png"
+  }
+]);
